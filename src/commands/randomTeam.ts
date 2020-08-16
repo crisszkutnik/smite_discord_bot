9f7797c3456a94d35013d5da0f7c49307ac5d3d3
cristobalszk <crisszkutnik@gmail.com>
2020-08-16 15:43:22 -0300

    Have to rework randomTeam command

--- src/commands/randomTeam.ts
+++ src/commands/randomTeam.ts
@@ -6,37 +6,58 @@ const errorFound = (message) => {
     message.channel.send("Error encontrado. La sintaxis es: `+rteam [2-5]? (all? [class1-class5]+)?`");
 }
 
-const checkSyntax = (num_str, number, all, classes) => {
-    if(number && (number < 2 && number > 5))
-        return false;
-    
-    if(num_str === "all") {
-        let isOk = classRegExp.test(all);
-
-        for(let i = 0; i < classes.length && isOk; i++)
-            isOk = classRegExp.test(classes[i]);
-
-        return isOk;
+const checkSyntax = (num_str, number, all, classes):Boolean => {
+    if(!number) {
+        if(num_str) {
+            if(num_str === "all")
+                all = "all";
+            else if(classRegExp.test(num_str))
+                classes.push(num_str);
+            else
+                return false;
+        } else
+            return true;
     } else {
+        if(number < 2 || number > 5)
+            return false;
 
+        /*if(all !== "all" && classRegExp.test(all))
+            classes.push(all);
+        else
+            return false;*/
+        if(all && all !== "all") {
+            if(classRegExp.test(all))
+                classes.push(all);
+            else
+                return false;
+        }
     }
+
+    for(let i = 0; i < classes.length; i++)
+        if(!classRegExp.test(classes[i]))
+            return false;
+
+    return true;
 }
 
 module.exports = {
     name: "randomTeam",
-    execute(message, [num_str, all, ...classes]:[string, string, string]) {
+    execute(message, [num_str, all, ...classes]) {
         let number = parseInt(num_str);
 
-        /*if(!checkSyntax(num_str, number, all, classes)) {
+        if(!checkSyntax(num_str, number, all, classes)) {
             errorFound(message);
             return;
-        }*/
+        }
+
+        let gods = [];
 
         if(!number) {
-            // rteam all [classes] || rteam classes
             number = 5;
 
-            if(num_str === "all")
+            if(!num_str)
+                classes = ["guardian", "jg", "mid", "warrior", "adc"];
+            else if(num_str === "all")
                 classes.push(all);
             else {
                 classes.push(num_str);
@@ -44,8 +65,45 @@ module.exports = {
                 if(all)
                     classes.push(all);
             }
+        } else
+            if(all)
+                classes.push(all);
 
-            all = "all";
+
+
+        let god_str = '';
+
+        gods.forEach(e => god_str += e + '\n');
+
+        message.channel.send(god_str);
+    }
+}
+
+/*
+This code is strange
+*/
+
+/*
+if(!number) {
+            // rteam all [classes] || rteam classes
+            number = 5;
+
+            if(!num_str) {
+                classes = ["guardian","warrior", "jg", "mid"];
+
+                all = "adc";
+            } else {
+                if(num_str === "all")
+                    classes.push(all);
+                else {
+                    classes.push(num_str);
+
+                if(all)
+                    classes.push(all);
+                }
+
+                all = "all";
+            }
         }
 
         for(let i = 0; i < classes.length; i++)
@@ -82,15 +140,4 @@ module.exports = {
                 gods.push(god);
             }
         }
-
-        let god_str = '';
-
-        gods.forEach(e => god_str += e + '\n');
-
-        message.channel.send(god_str);
-    }
-}
-
-/*
-SOLVE SYNTAX CHECKER
 */
